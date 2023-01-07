5e901d7ccf406c452906fa7109ad0580404bce54
LLagoon3 <choonarm3@gmail.com>
2023-01-07 16:05:01 +0900

    MOD DATA

--- Swift_Basic/main.swift
+++ Swift_Basic/main.swift
@@ -7,22 +7,30 @@
 
 import Foundation
 
-//변수와 상수
+
+///변수와 상수
+
 var name = "LeeSeokHo"
 let birth_year = 2000
 
-//정적 타이핑
+
+///정적 타이핑
+
 var name_ : String = "mark"
 let birth_year_ : Int = 1990
 var hieght : Float = 170.3
 //print(Float(birth_year_) + hieght)
 //print(String(birth_year_) + ":" + name_)
 
-//타입추론
+
+///타입추론
+
 var name__ = "mark"
 //print(name__)
 
-//배열, 딕셔너리
+
+///배열, 딕셔너리
+
 var language = ["swift", "C", "python"]
 var capital = [
     "한국" : "서울",
@@ -30,7 +38,9 @@ var capital = [
 ]
 //print(capital["한국"])
 
-//조건문
+
+///조건문
+
 var num = 0
 if num == 0{
 //    print(num)
@@ -52,10 +62,10 @@ if num.words.isEmpty{
 
 //optional
 
-var email : String? = "mark"
+var email : String? = ""
 //print(email)
 
-if let name = email{
+if let name : String = email{
 //    print(name)
 }
 
@@ -64,6 +74,107 @@ var isEmptyarr : Bool?
 
 isEmptyarr = arr?.isEmpty == true
 
-print(isEmptyarr!)
+//print(isEmptyarr!)
 
 
+///func
+
+func hello(_ name : String?) -> String {
+    if name == nil{return "hello"}
+    else{return(name! + "hello")}
+}
+//print(hello(""))
+
+func hello_generator() -> (String?) -> Int{
+    func hello_(name : String?) -> Int{
+        if let _ = name{
+            return 0
+        }
+        else{
+            return 1
+        }
+    }
+    return hello_
+}
+
+//var hello = hello_generator()
+//print(hello(nil))
+
+
+///closure
+
+func hello_generator_() -> (String?) -> Bool?{
+    return {name in
+        return name == nil
+        }
+}
+//print(hello_generator_("hi"))
+
+func hello_generator__() -> (String?) -> Bool?{
+    return {$0 == nil}
+}
+//print(hello_generator__()(nil))
+
+let hello_ : ((String?) -> Any) = {$0 == nil}
+//print(hello_(nil))
+
+var arr_ = [1, 2, 3, 4, 5]
+//print(arr_.reduce(100){$0 - $1})
+
+
+///struct, class
+
+class Dog{
+    var name : String?
+    func hello(){
+        print(self.name)
+    }}
+
+struct Cat{
+    var name : String
+    init(){
+        self.name = "mark"
+    }
+    func hello(){
+    print(self.name)
+    }
+}
+
+var dog = Dog()
+dog.name = nil
+//dog.hello()
+
+var cat = Cat()
+cat.name = "lee"
+//cat.hello()
+//클래스는 참조 구조체는 복사
+
+
+///property
+
+class hex{
+    var decimal : Int?
+    var str : String?{
+        get{
+            if let _ = self.decimal{
+                return String(self.decimal!, radix: 16)
+            }
+            else{
+                return nil
+            }
+        }
+        set{
+            if let tmp = newValue{
+                self.decimal = Int(tmp)
+            }
+            else{
+                self.decimal = nil
+            }
+        }
+    }
+}
+
+var test = hex()
+test.str = "15"
+//print(test.decimal)
+//print(test.str)
